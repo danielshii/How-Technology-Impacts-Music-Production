759f2d7cb2729fe0e511bffd6f3b041859e9acb1
danielshii <danielshi0516@gmail.com>
2022-12-06 14:47:33 -0500

    give iframes unique title

--- src/components/pages/PageThree.js
+++ src/components/pages/PageThree.js
@@ -1,5 +1,5 @@
 import references from "../../static/ShiReferences01.htm";
-import images from "../../static/ShiImages.htm"
+import images from "../../static/ShiImages.htm";
 import Conclusion from "../Conclusion";
 
 function PageThree() {
@@ -7,8 +7,8 @@ function PageThree() {
     <div className="page">
       <Conclusion />
       <div className="bib-container">
-        <iframe frameBorder={0} src={references}></iframe>
-        <iframe frameBorder={0} src={images}></iframe>
+        <iframe title="references" frameBorder={0} src={references}></iframe>
+        <iframe title="images" frameBorder={0} src={images}></iframe>
       </div>
     </div>
   );
