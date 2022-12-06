1b34cd9d1f1ca8e3873f9f6e9a5a82e3af9c2410
danielshii <danielshi0516@gmail.com>
2022-12-06 15:02:16 -0500

    fixed reload issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import PageOne from "./components/pages/PageOne";
 import "./fonts/OldEnglish.ttf";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useHref, useNavigate } from "react-router-dom";
 import PageTwo from "./components/pages/PageTwo";
 import { useState } from "react";
 import PageThree from "./components/pages/PageThree";
@@ -9,13 +9,17 @@ import PageThree from "./components/pages/PageThree";
 const PAGES = 3;
 
 function App() {
-  const [currentPage, setCurrentPage] = useState(1);
+  const h = useHref();
+  // console.log(h);
+  const [currentPage, setCurrentPage] = useState(
+    h === "/" ? 1 : parseInt(h.slice(-1))
+  );
 
   const nav = useNavigate();
 
   function incrementPage() {
     if (currentPage < PAGES) {
-      nav(`/${currentPage + 1}`);
+      nav(`/page/${currentPage + 1}`);
       setCurrentPage(currentPage + 1);
       window.scrollTo(0, 0);
     }
@@ -23,7 +27,7 @@ function App() {
 
   function decrementPage() {
     if (currentPage > 1) {
-      nav(`/${currentPage - 1}`);
+      nav(`/page/${currentPage - 1}`);
       setCurrentPage(currentPage - 1);
       window.scrollTo(0, 0);
     }
@@ -40,9 +44,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<PageOne />} />
-        <Route path="/1" element={<PageOne />} />
-        <Route path="/2" element={<PageTwo />} />
-        <Route path="/3" element={<PageThree />} />
+        <Route path="/page/1" element={<PageOne />} />
+        <Route path="/page/2" element={<PageTwo />} />
+        <Route path="/page/3" element={<PageThree />} />
       </Routes>
     </div>
   );
